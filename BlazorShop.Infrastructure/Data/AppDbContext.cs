d461a2a2351060d3ea65c2bd583ea653d99f2d71
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-05 22:05:16 +0200

    Add AppDbContext with DbSets for Categories and Products
    Introduce a new namespace `BlazorShop.Infrastructure.Data`.
    Define `AppDbContext` class inheriting from `DbContext`.
    Add constructor accepting `DbContextOptions<AppDbContext>`.
    Include `DbSet<Category>` and `DbSet<Product>` properties.
    Add `using` directives for necessary namespaces.

---
+++ BlazorShop.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorShop.Infrastructure.Data
+{
+    using BlazorShop.Domain.Entities;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
