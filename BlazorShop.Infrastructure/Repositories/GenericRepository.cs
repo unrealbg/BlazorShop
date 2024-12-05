4d6d897b47226496ccd4f04e5b5aafe0c69ae884
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-05 22:03:45 +0200

    Introduce generic repository pattern to BlazorShop
    Added IGenericRepository interface in BlazorShop.Domain.Contracts with methods for common data operations. Implemented GenericRepository class in BlazorShop.Infrastructure.Repositories using Entity Framework Core for CRUD operations.

---
+++ BlazorShop.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace BlazorShop.Infrastructure.Repositories
+{
+    using BlazorShop.Application.Exceptions;
+    using BlazorShop.Domain.Contracts;
+    using BlazorShop.Infrastructure.Data;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _ctx;
+
+        public GenericRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<int> AddAsync(TEntity entity)
+        {
+            _ctx.Set<TEntity>().Add(entity);
+
+            return await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteAsync(Guid id)
+        {
+            var entity = await this.GetByIdAsync(id) ?? throw new ItemNotFoundException(id);
+
+            _ctx.Set<TEntity>().Remove(entity);
+            return await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _ctx.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _ctx.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<int> UpdateAsync(TEntity entity)
+        {
+            _ctx.Set<TEntity>().Update(entity);
+            return await _ctx.SaveChangesAsync();
+        }
+    }
+}
