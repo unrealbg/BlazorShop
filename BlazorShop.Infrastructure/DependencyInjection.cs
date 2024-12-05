23625bf854fe8a2afdfb9f32a678e344a4cd6dba
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-05 22:04:05 +0200

    Add DependencyInjection classes for Application and Infrastructure
    Two new static classes named `DependencyInjection` have been added to the `BlazorShop.Application` and `BlazorShop.Infrastructure` namespaces.
    
    In the `BlazorShop.Application` namespace, the `DependencyInjection` class includes a method `AddApplication` that extends `IServiceCollection` to return the service collection.
    
    In the `BlazorShop.Infrastructure` namespace, the `DependencyInjection` class includes a method `AddInfrastructure` that extends `IServiceCollection` to add a scoped service for `IGenericRepository<>` with an implementation of `GenericRepository<>`, and then returns the service collection.

---
+++ BlazorShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorShop.Infrastructure
+{
+    using BlazorShop.Domain.Contracts;
+    using BlazorShop.Infrastructure.Repositories;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            return services;
+        }
+    }
+}
