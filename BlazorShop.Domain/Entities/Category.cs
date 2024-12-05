03b08eaf383327017ef6d20dc385530ff449a23c
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-05 22:05:03 +0200

    Add Category and Product entity classes
    Introduce two new entity classes, `Category` and `Product`, in the `BlazorShop.Domain.Entities` namespace. The `Category` class includes properties for `Id`, `Name`, and a collection of `Products`. The `Product` class includes properties for `Id`, `Name`, `Description`, `Price`, `Image`, `Quantity`, `CategoryId`, and a reference to a `Category`. Both classes utilize data annotations from the `System.ComponentModel.DataAnnotations` namespace.

---
+++ BlazorShop.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Domain.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ BlazorShop.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorShop.Domain.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Image { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
