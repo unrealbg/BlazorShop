a18dacdd7b55f451e125770011bd1a4b9b9c6753
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-11 20:36:05 +0200

    Add Category and Product controllers
    This commit introduces two new controller classes: `CategoryController` and `ProductController` in the `BlazorShop.API.Controllers` namespace.
    
    The `CategoryController` handles HTTP requests for categories, including methods for retrieving, adding, updating, and deleting categories using the `ICategoryService`.
    
    The `ProductController` manages HTTP requests for products with similar functionality, utilizing the `IProductService`.
    
    Both controllers are set up with appropriate routing and API behavior through the `[Route("api/[controller]")]` and `[ApiController]` attributes.

---
+++ BlazorShop.API/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿namespace BlazorShop.API.Controllers
+{
+    using BlazorShop.Application.DTOs.Category;
+    using BlazorShop.Application.Services.Contracts;
+    using BlazorShop.Domain.Entities;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return categories.Any() ? this.Ok(categories) : this.NotFound(categories);
+        }
+
+        [HttpGet("single/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return category != null ? this.Ok(category) : this.NotFound(category);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(CreateCategory category)
+        {
+            var result = await _categoryService.AddAsync(category);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateCategory category)
+        {
+            var result = await _categoryService.UpdateAsync(category);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _categoryService.DeleteAsync(id);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+    }
+}
---
+++ BlazorShop.API/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿namespace BlazorShop.API.Controllers
+{
+    using BlazorShop.Application.DTOs.Product;
+    using BlazorShop.Application.Services.Contracts;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _productService.GetAllAsync();
+            return data.Any() ? this.Ok(data) : this.NotFound(data);
+        }
+
+        [HttpGet("single/{id}")]
+        public async Task<IActionResult> GetSingle(Guid id)
+        {
+            var data = await _productService.GetByIdAsync(id);
+            return data != null ? this.Ok(data) : this.NotFound(data);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(CreateProduct product)
+        {
+            var result = await _productService.AddAsync(product);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateProduct product)
+        {
+            var result = await _productService.UpdateAsync(product);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _productService.DeleteAsync(id);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+    }
+}
