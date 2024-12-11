c1930a69d22bd041074d8b698cb06e4f4d3cad05
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-11 20:35:13 +0200

    Add GetCategory and GetProduct classes
    Introduce `GetCategory` and `GetProduct` classes in their respective namespaces.
    `GetCategory` includes an `Id` and a collection of `GetProduct` objects, while
    `GetProduct` has a required `Id` and a reference to `GetCategory`.
    These changes establish a relationship between categories and products,
    enhancing data organization and retrieval.

---
+++ BlazorShop.Application/DTOs/Category/GetCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.Application.DTOs.Category
+{
+    using BlazorShop.Application.DTOs.Product;
+
+    public class GetCategory : CategoryBase
+    {
+        public Guid Id { get; set; }
+
+        public ICollection<GetProduct>? Products { get; set; }
+    }
+}
+ 
---
+++ BlazorShop.Application/DTOs/Product/GetProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Application.DTOs.Product
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using BlazorShop.Application.DTOs.Category;
+
+    public class GetProduct : ProductBase
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        public GetCategory? Category { get; set; }
+    }
+}
