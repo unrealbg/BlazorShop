386b9128d57623eb9e5b8b34fcc6bc667b5824c9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-12 17:51:16 +0200

    Add AppUser and RefreshToken classes
    Introduces `AppUser` extending `IdentityUser` with a
    `FullName` property, and `RefreshToken` with properties
    for `Id`, `UserId`, and `Token`, including default values.

---
+++ BlazorShop.Domain/Entities/Identity/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.Domain.Entities.Identity
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string UserId { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+    }
+}
