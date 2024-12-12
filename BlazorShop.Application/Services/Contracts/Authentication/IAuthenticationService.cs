a6803b95b6aa412d7ab9a570fff069ff15c7270d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-12 17:50:19 +0200

    Add authentication and logging service interfaces
    Updated `IAuthenticationService.cs` to define the
    `IAuthenticationService` interface with methods for
    user creation, login, and token revival.
    
    Modified `IAppLogger.cs` to introduce the
    `IAppLogger<T>` interface with methods for logging
    information, warnings, and errors.

---
+++ BlazorShop.Application/Services/Contracts/Authentication/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Application.Services.Contracts.Authentication
+{
+    using BlazorShop.Application.DTOs;
+    using BlazorShop.Application.DTOs.UserIdentity;
+
+    public interface IAuthenticationService
+    {
+        Task<ServiceResponse> CreateUser(CreateUser request);
+
+        Task<LoginResponse> LoginUser(LoginUser user);
+
+        Task<LoginResponse> ReviveToken(string refreshToken);
+    }
+}
---
+++ BlazorShop.Application/Services/Contracts/Logging/IAppLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.Application.Services.Contracts.Logging
+{
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message);
+
+        void LogWarning(string message);
+
+        void LogError(Exception exception, string message);
+    }
+}
