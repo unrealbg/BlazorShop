cffac503f6cb2118c952cd3016a6b97dc1c1105d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-12 17:51:04 +0200

    Add LoggerAdapter class for logging functionality
    Introduces `LoggerAdapter<T>` in the `BlazorShop.Infrastructure.Services` namespace. This class implements the `IAppLogger<T>` interface and provides methods for logging information, warnings, and errors using an `ILogger<T>` instance. The methods `LogInformation`, `LogWarning`, and `LogError` are included to facilitate logging operations.

---
+++ BlazorShop.Infrastructure/Services/LoggerAdapter.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorShop.Infrastructure.Services
+{
+    using BlazorShop.Application.Services.Contracts.Logging;
+
+    using Microsoft.Extensions.Logging;
+
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+        public void LogError(Exception exception, string message)
+        {
+            _logger.LogError(exception, message);
+        }
+    }
+}
