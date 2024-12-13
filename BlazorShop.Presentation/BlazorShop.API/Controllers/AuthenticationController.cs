45680597d7694f5fdfdfb8a1efd98f5fa4404ff9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-13 17:45:00 +0200

    Add AuthenticationController for user management
    Introduces the AuthenticationController class in the BlazorShop.API.Controllers namespace. This controller handles user authentication actions, including creating users, logging in, and refreshing tokens. It leverages the IAuthenticationService for asynchronous operations and defines three HTTP endpoints for these functionalities, returning appropriate responses based on the success of the operations.

---
+++ BlazorShop.Presentation/BlazorShop.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorShop.API.Controllers
+{
+    using BlazorShop.Application.DTOs.UserIdentity;
+    using BlazorShop.Application.Services.Contracts.Authentication;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateUser(CreateUser user)
+        {
+            var result = await _authenticationService.CreateUser(user);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(LoginUser user)
+        {
+            var result = await _authenticationService.LoginUser(user);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+        [HttpGet("refreshToken/{refreshToken}")]
+        public async Task<IActionResult> RefreshToken(string refreshToken)
+        {
+            var result = await _authenticationService.ReviveToken(refreshToken);
+            return result.Success ? this.Ok(result) : this.BadRequest(result);
+        }
+
+    }
+}
