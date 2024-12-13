41e30ceae6f0a8f8d0c673e0fd267b09cabd8ebb
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-13 17:42:14 +0200

    Add user validation classes and validation service
    Introduces `CreateUserValidator` and `LoginUserValidator` using FluentValidation to enforce input rules for user creation and login. Implements `IValidationService` for asynchronous validation, with `ValidationService` handling validation results and structured response messages.

---
+++ BlazorShop.Application/Validations/Authentication/CreateUserValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorShop.Application.Validations.Authentication
+{
+    using BlazorShop.Application.DTOs.UserIdentity;
+
+    using FluentValidation;
+
+    public class CreateUserValidator : AbstractValidator<CreateUser>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.FullName).NotEmpty().WithMessage("Full Name is required.");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email is not valid.");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"\d").WithMessage("Password must contain at least one digit.")
+                .Matches(@"[^\w]").WithMessage("Password must contain at least one special character.");
+
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage("Passwords do not match.");
+        }
+    }
+}
