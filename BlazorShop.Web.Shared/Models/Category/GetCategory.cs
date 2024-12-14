3d2dd8b64566be26092ba392ac58634f224c666b
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:34:58 +0200

    Add category models with inheritance and properties
    Introduces the `BlazorShop.Web.Shared.Models.Category` namespace containing the `CategoryBase`, `CreateCategory`, `UpdateCategory`, and `GetCategory` classes. The `CategoryBase` class includes a required `Name` property, while `UpdateCategory` adds a required `Id`. The `GetCategory` class features an `Id` property and a collection of `GetProduct` objects for retrieving category details and associated products.

---
+++ BlazorShop.Web.Shared/Models/Category/GetCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Web.Shared.Models.Category
+{
+    public class GetCategory : CategoryBase
+    {
+        public Guid Id { get; set; }
+        
+        public ICollection<GetProduct>? Products { get; set; }
+    }
+}
