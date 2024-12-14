b2a33d830ea1224d443e4a6b41ac8f0c735077d4
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:03:36 +0200

    Add CartService and PaymentMethodService classes
    Implemented CartService for managing checkout history and payment processing. Added PaymentMethodService to retrieve and map available payment methods. Both services utilize dependency injection for their dependencies.

---
+++ BlazorShop.Application/Services/Payment/CartService.cs
@@ -0,0 +1,84 @@
+﻿namespace BlazorShop.Application.Services.Payment
+{
+    using AutoMapper;
+
+    using BlazorShop.Application.DTOs;
+    using BlazorShop.Application.DTOs.Payment;
+    using BlazorShop.Application.Services.Contracts.Payment;
+    using BlazorShop.Domain.Contracts;
+    using BlazorShop.Domain.Contracts.Payment;
+    using BlazorShop.Domain.Entities;
+    using BlazorShop.Domain.Entities.Payment;
+
+    public class CartService : ICartService
+    {
+        private readonly ICart _cart;
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IPaymentMethodService _paymentMethodService;
+        private readonly IPaymentService _paymentService;
+
+        public CartService(ICart cart,
+                           IMapper mapper,
+                           IGenericRepository<Product> productRepository,
+                           IPaymentMethodService paymentMethodService,
+                           IPaymentService paymentService)
+        {
+            this._cart = cart;
+            this._mapper = mapper;
+            this._productRepository = productRepository;
+            this._paymentMethodService = paymentMethodService;
+            this._paymentService = paymentService;
+        }
+
+        public async Task<ServiceResponse> SaveCheckoutHistoryAsync(IEnumerable<CreateAchieve> achieves)
+        {
+            var mappedData = this._mapper.Map<IEnumerable<Achieve>>(achieves);
+            var result = await this._cart.SaveCheckoutHistory(mappedData);
+
+            return result > 0 ? new ServiceResponse(true, "Checkout history saved successfully") : new ServiceResponse(false, "Failed to save checkout history");
+        }
+
+        public async Task<ServiceResponse> CheckoutAsync(Checkout checkout)
+        {
+            var (products, totalAmount) = await this.GetCartTotalAmount(checkout.Carts);
+            var paymentMethods = await this._paymentMethodService.GetPaymentMethodsAsync();
+
+            if (checkout.PaymentMethodId == paymentMethods.FirstOrDefault()!.Id)
+            {
+                return await this._paymentService.Pay(totalAmount, products, checkout.Carts);
+            }
+
+            return new ServiceResponse(false, "Invalid payment method");
+        }
+
+        private async Task<(IEnumerable<Product>, decimal)> GetCartTotalAmount(IEnumerable<ProcessCart> carts)
+        {
+            if (!carts.Any())
+            {
+                return ([], 0);
+            }
+
+            var products = await this._productRepository.GetAllAsync();
+
+            if (!products.Any())
+            {
+                return ([], 0);
+            }
+
+            var cartProducts = carts
+                .Select(ci => products
+                    .FirstOrDefault(p => p.Id == ci.ProductId))
+                .Where(p => p != null)
+                .ToList();
+
+            var totalAmount = carts
+                .Where(ci => cartProducts.Any(p => p.Id == ci.ProductId))
+                .Sum(ci => ci.Quantity * cartProducts
+                    .First(p => p.Id == ci.ProductId)!
+                    .Price);
+
+            return (cartProducts!, totalAmount);
+        }
+    }
+}
---
+++ BlazorShop.Application/Services/Payment/PaymentMethodService.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorShop.Application.Services.Payment
+{
+    using AutoMapper;
+
+    using BlazorShop.Application.DTOs.Payment;
+    using BlazorShop.Application.Services.Contracts.Payment;
+    using BlazorShop.Domain.Contracts.Payment;
+
+    public class PaymentMethodService : IPaymentMethodService
+    {
+        private readonly IPaymentMethod _paymentMethod;
+        private readonly IMapper _mapper;
+
+        public PaymentMethodService(IPaymentMethod paymentMethod, IMapper mapper)
+        {
+            this._paymentMethod = paymentMethod;
+            this._mapper = mapper;
+        }
+
+        public async Task<IEnumerable<GetPaymentMethod>> GetPaymentMethodsAsync()
+        {
+            var methods = await this._paymentMethod.GetPaymentMethodsAsync();
+
+            if (!methods.Any())
+            {
+                return [];
+            }
+
+            return this._mapper.Map<IEnumerable<GetPaymentMethod>>(methods);
+        }
+    }
+}
