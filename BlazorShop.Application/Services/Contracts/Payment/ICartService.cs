ab14b7f8b5927166071183f9c20cbb495a98dc4f
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:02:50 +0200

    Add payment and cart service interfaces
    Introduce `ICartService`, `IPaymentMethodService`, and `IPaymentService` interfaces in the `BlazorShop.Application.Services.Contracts.Payment` namespace. These interfaces provide methods for managing checkout history, retrieving payment methods, and processing payments.

---
+++ BlazorShop.Application/Services/Contracts/Payment/ICartService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.Application.Services.Contracts.Payment
+{
+    using BlazorShop.Application.DTOs;
+    using BlazorShop.Application.DTOs.Payment;
+
+    public interface ICartService
+    {
+        Task<ServiceResponse> SaveCheckoutHistoryAsync(IEnumerable<CreateAchieve> achieves);
+
+        Task<ServiceResponse> CheckoutAsync(Checkout checkout);
+    }
+}
