389ae977a2abdffe13e0da0669bc27eabf9ad44a
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:04:54 +0200

    Add PaymentMethod and CheckoutAchieve to AppDbContext
    - Introduced `PaymentMethod` and `CheckoutAchieves` DbSets.
    - Seeded default data for `PaymentMethod` in OnModelCreating.
    - Updated `IdentityRole` seeding to use object initializers for clarity.

--- BlazorShop.Infrastructure/Data/AppDbContext.cs
+++ BlazorShop.Infrastructure/Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 {
     using BlazorShop.Domain.Entities;
     using BlazorShop.Domain.Entities.Identity;
+    using BlazorShop.Domain.Entities.Payment;
 
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -20,23 +21,34 @@
 
         public DbSet<RefreshToken> RefreshTokens { get; set; }
 
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+
+        public DbSet<Achieve> CheckoutAchieves { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            builder.Entity<PaymentMethod>().HasData(
+                new PaymentMethod
+                {
+                    Id = Guid.Parse("3604fc1d-cd6a-46ad-ace4-9b5f8e03f43b"),
+                    Name = "Credit Card",
+                });
+
             builder.Entity<IdentityRole>().HasData(
                 new IdentityRole
-                {
-                    Id = "93f5cdac-43de-4895-8426-2048c228e76d",
-                    Name = "Admin",
-                    NormalizedName = "ADMIN"
-                },
+                    {
+                        Id = "93f5cdac-43de-4895-8426-2048c228e76d",
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    },
                 new IdentityRole
-                {
-                    Id = "b7af6842-02fa-4af4-8f61-ae04a49644a2",
-                    Name = "User",
-                    NormalizedName = "USER"
-                });
+                    {
+                        Id = "b7af6842-02fa-4af4-8f61-ae04a49644a2",
+                        Name = "User",
+                        NormalizedName = "USER"
+                    });
         }
     }
 }
