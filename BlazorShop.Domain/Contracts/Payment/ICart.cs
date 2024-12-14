5e49a53822baa2d955ba74f10bc3e5f390be8df3
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:03:03 +0200

    Add ICart and IPaymentMethod interfaces
    Introduced two new interfaces in the BlazorShop.Domain.Contracts.Payment namespace.
    The ICart interface includes a SaveCheckoutHistory method for handling checkout history,
    while the IPaymentMethod interface provides a method to retrieve available payment methods asynchronously.

---
+++ BlazorShop.Domain/Contracts/Payment/ICart.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Domain.Contracts.Payment
+{
+    using BlazorShop.Domain.Entities.Payment;
+
+    public interface ICart
+    {
+        Task<int> SaveCheckoutHistory(IEnumerable<Achieve> checkouts);
+    }
+}
---
+++ BlazorShop.Domain/Contracts/Payment/IPaymentMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Domain.Contracts.Payment
+{
+    using BlazorShop.Domain.Entities.Payment;
+
+    public interface IPaymentMethod
+    {
+        Task<IEnumerable<PaymentMethod>> GetPaymentMethodsAsync();
+    }
+}
