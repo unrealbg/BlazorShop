98e98542da440f884b74975b4786e5696354a147
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:04:02 +0200

    Add Cart and Payment controllers for API management
    This commit introduces two new API controllers: `CartController` and `PaymentController`.
    The `CartController` manages cart operations with endpoints for checking out and saving checkout history, utilizing the `ICartService`.
    The `PaymentController` handles payment methods, providing an endpoint to retrieve all available payment methods through the `IPaymentMethodService`.

---
+++ BlazorShop.Presentation/BlazorShop.API/Controllers/PaymentController.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorShop.API.Controllers
+{
+    using BlazorShop.Application.DTOs.Payment;
+    using BlazorShop.Application.Services.Contracts.Payment;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentMethodService _paymentMethodService;
+
+        public PaymentController(IPaymentMethodService paymentMethodService)
+        {
+            _paymentMethodService = paymentMethodService;
+        }
+
+        /// <summary>
+        /// Get all payment methods
+        /// </summary>
+        /// <returns>The payment methods </returns>
+        [HttpGet("payment-methods")]
+        public async Task<ActionResult<IEnumerable<GetPaymentMethod>>> GetPaymentMethods()
+        {
+            var paymentMethods = await _paymentMethodService.GetPaymentMethodsAsync();
+            return !paymentMethods.Any() ? this.NotFound() : this.Ok(paymentMethods);
+        }
+    }
+}
