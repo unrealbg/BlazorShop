0c0f604c0953e344444aaf389ef0da32bc3607bf
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-14 21:35:08 +0200

    Add product models for creation, retrieval, and update
    Introduces the `BlazorShop.Web.Shared.Models.Product` namespace with four classes: `CreateProduct`, `GetProduct`, `ProductBase`, and `UpdateProduct`.
    
    - `CreateProduct` and `UpdateProduct` inherit from `ProductBase`, providing a structure for creating and updating products.
    - `GetProduct` includes properties for product details, including a computed `IsNew` property to check if the product was created within the last 7 days.
    - `ProductBase` defines common required properties for products such as `Name`, `Description`, `ImageUrl`, `Price`, `Quantity`, and `CategoryId`.

---
+++ BlazorShop.Web.Shared/Models/Product/GetProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorShop.Web.Shared.Models.Product
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using BlazorShop.Web.Shared.Models.Category;
+
+    public class GetProduct : ProductBase
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        public GetCategory? Category { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsNew => DateTime.UtcNow.Subtract(this.CreatedOn).TotalDays <= 7;
+    }
+}
---
+++ BlazorShop.Web.Shared/Models/Product/ProductBase.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorShop.Web.Shared.Models.Product
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductBase
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public Guid CategoryId { get; set; }
+    }
+}
