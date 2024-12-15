9bfbd5ba1a6fd0df26f8e20c3270bcca61b81315
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-15 20:27:40 +0200

    Add services for authentication, category, and product
    Implemented `AuthenticationService`, `CategoryService`, and `ProductService` with their respective interfaces. Each service includes methods for creating, retrieving, updating, and deleting users, categories, and products. Introduced `IApiCallHelper` and `IHttpClientHelper` for API calls, incorporating error handling and asynchronous patterns for improved performance.

---
+++ BlazorShop.Presentation/BlazorShop.Web.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,75 @@
+﻿namespace BlazorShop.Web.Shared.Services
+{
+    using System.Web;
+
+    using BlazorShop.Web.Shared.Helper.Contracts;
+    using BlazorShop.Web.Shared.Models;
+    using BlazorShop.Web.Shared.Models.Authentication;
+    using BlazorShop.Web.Shared.Services.Contracts;
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IHttpClientHelper _httpClientHelper;
+        private readonly IApiCallHelper _apiCallHelper;
+
+        public AuthenticationService(IHttpClientHelper httpClientHelper, IApiCallHelper apiCallHelper)
+        {
+            _httpClientHelper = httpClientHelper;
+            _apiCallHelper = apiCallHelper;
+        }
+
+        public async Task<ServiceResponse> CreateUser(CreateUser user)
+        {
+            var client = await _httpClientHelper.GetPrivateClientAsync();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Authentication.Register,
+                Type = Constant.ApiCallType.Post,
+                Client = client,
+                Id = null!,
+                Model = user,
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<CreateUser>(currentApiCall);
+            return result == null
+                       ? _apiCallHelper.ConnectionError()
+                       : await _apiCallHelper.GetServiceResponse<ServiceResponse>(result);
+        }
+
+        public async Task<LoginResponse> LoginUser(LoginUser user)
+        {
+            var client = await _httpClientHelper.GetPrivateClientAsync();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Authentication.Login,
+                Type = Constant.ApiCallType.Post,
+                Client = client,
+                Id = null!,
+                Model = user,
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<LoginUser>(currentApiCall);
+            return result == null
+                       ? new LoginResponse(Message: this._apiCallHelper.ConnectionError().Message)
+                       : await _apiCallHelper.GetServiceResponse<LoginResponse>(result);
+        }
+
+        public async Task<LoginResponse> ReviveToken(string refreshToken)
+        {
+            var client = _httpClientHelper.GetPublicClient();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Authentication.ReviveToke,
+                Type = Constant.ApiCallType.Get,
+                Client = client,
+                Model = null!,
+                Id = HttpUtility.UrlEncode(refreshToken),
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<Unit>(currentApiCall);
+            return result == null
+                       ? null!
+                       : await this._apiCallHelper.GetServiceResponse<LoginResponse>(result);
+        }
+    }
+}
---
+++ BlazorShop.Presentation/BlazorShop.Web.Shared/Services/ProductService.cs
@@ -0,0 +1,108 @@
+﻿namespace BlazorShop.Web.Shared.Services
+{
+    using BlazorShop.Web.Shared.Helper.Contracts;
+    using BlazorShop.Web.Shared.Models;
+    using BlazorShop.Web.Shared.Models.Product;
+    using BlazorShop.Web.Shared.Services.Contracts;
+
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientHelper _httpClientHelper;
+        private readonly IApiCallHelper _apiCallHelper;
+
+        public ProductService(IHttpClientHelper httpClientHelper, IApiCallHelper apiCallHelper)
+        {
+            _httpClientHelper = httpClientHelper;
+            _apiCallHelper = apiCallHelper;
+        }
+
+        public async Task<IEnumerable<GetProduct>> GetAllAsync()
+        {
+            var client = _httpClientHelper.GetPublicClient();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Product.Get,
+                Type = Constant.ApiCallType.Get,
+                Client = client,
+                Model = null!,
+                Id = null!
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<Unit>(currentApiCall);
+            return result == null
+                       ? []
+                       : await this._apiCallHelper.GetServiceResponse<IEnumerable<GetProduct>>(result);
+        }
+
+        public async Task<GetProduct> GetByIdAsync(Guid id)
+        {
+            var client = _httpClientHelper.GetPublicClient();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Product.Get,
+                Type = Constant.ApiCallType.Get,
+                Client = client,
+                Model = null!,
+            };
+
+            currentApiCall.ToString(id);
+            var result = await _apiCallHelper.ApiCallTypeCall<Unit>(currentApiCall);
+            return result == null
+                       ? null!
+                       : await this._apiCallHelper.GetServiceResponse<GetProduct>(result);
+        }
+
+        public async Task<ServiceResponse> AddAsync(CreateProduct product)
+        {
+            var client = await _httpClientHelper.GetPrivateClientAsync();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Product.Add,
+                Type = Constant.ApiCallType.Post,
+                Client = client,
+                Id = null!,
+                Model = product,
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<CreateProduct>(currentApiCall);
+            return result == null
+                       ? this._apiCallHelper.ConnectionError()
+                       : await this._apiCallHelper.GetServiceResponse<ServiceResponse>(result);
+        }
+
+        public async Task<ServiceResponse> UpdateAsync(UpdateProduct product)
+        {
+            var client = await _httpClientHelper.GetPrivateClientAsync();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Product.Update,
+                Type = Constant.ApiCallType.Update,
+                Client = client,
+                Id = null!,
+                Model = product,
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<UpdateProduct>(currentApiCall);
+            return result == null
+                       ? this._apiCallHelper.ConnectionError()
+                       : await this._apiCallHelper.GetServiceResponse<ServiceResponse>(result);
+        }
+
+        public async Task<ServiceResponse> DeleteAsync(Guid id)
+        {
+            var client = await _httpClientHelper.GetPrivateClientAsync();
+            var currentApiCall = new ApiCall
+            {
+                Route = Constant.Product.Delete,
+                Type = Constant.ApiCallType.Delete,
+                Client = client,
+                Model = null!,
+            };
+
+            var result = await _apiCallHelper.ApiCallTypeCall<Unit>(currentApiCall);
+            return result == null
+                       ? this._apiCallHelper.ConnectionError()
+                       : await this._apiCallHelper.GetServiceResponse<ServiceResponse>(result);
+        }
+    }
+}
