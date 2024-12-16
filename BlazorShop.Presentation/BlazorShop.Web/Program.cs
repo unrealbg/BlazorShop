1410d8759ac1e35f79d2a5e0ab166e63de7247aa
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-16 20:34:31 +0200

    Add services and authentication setup in Program.cs
    Updated `Program.cs` to include new using directives and register multiple services in the dependency injection container. Added support for cookie storage, token management, HTTP client helpers, product and category services, and authentication services. Configured a custom authentication state provider, refresh token handler, and an HTTP client with a base address for API calls. Enabled cascading authentication state and authorization core services.

--- BlazorShop.Presentation/BlazorShop.Web/Program.cs
+++ BlazorShop.Presentation/BlazorShop.Web/Program.cs
@@ -1,5 +1,15 @@
 namespace BlazorShop.Web
 {
+    using BlazorShop.Web.Authentication;
+    using BlazorShop.Web.Shared;
+    using BlazorShop.Web.Shared.CookieStorage;
+    using BlazorShop.Web.Shared.CookieStorage.Contracts;
+    using BlazorShop.Web.Shared.Helper;
+    using BlazorShop.Web.Shared.Helper.Contracts;
+    using BlazorShop.Web.Shared.Services;
+    using BlazorShop.Web.Shared.Services.Contracts;
+
+    using Microsoft.AspNetCore.Components.Authorization;
     using Microsoft.AspNetCore.Components.Web;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,7 +21,24 @@ namespace BlazorShop.Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddScoped<IBrowserCookieStorageService, BrowserCookieStorageService>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddScoped<IHttpClientHelper, HttpClientHelper>();
+            builder.Services.AddScoped<IApiCallHelper, ApiCallHelper>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddScoped<RefreshTokenHandler>();
+            builder.Services.AddHttpClient(
+                Constant.ApiClient.Name,
+                opt =>
+                    {
+                        opt.BaseAddress = new Uri("https://localhost:7094/api/");
+                    }).AddHttpMessageHandler<RefreshTokenHandler>();
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddAuthorizationCore();
 
             await builder.Build().RunAsync();
         }
