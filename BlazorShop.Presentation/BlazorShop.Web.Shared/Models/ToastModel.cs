eb8e5bd052858f3993dd961c1a93d2494b6147bc
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-19 14:40:01 +0200

    Add ToastModel class to manage toast notifications
    Introduced a new `ToastModel` class in the `BlazorShop.Web.Shared.Models` namespace. This class includes properties for `Heading`, `Message`, `ToastLevel`, `Icon`, `Position`, `Persist`, `Duration`, and `CancellationTokenSource`, all initialized with default values. The class also imports the `BlazorShop.Web.Shared.Toast` namespace.

---
+++ BlazorShop.Presentation/BlazorShop.Web.Shared/Models/ToastModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorShop.Web.Shared.Models
+{
+    using BlazorShop.Web.Shared.Toast;
+
+    public class ToastModel
+    {
+        public string Heading { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public string ToastLevel { get; set; } = string.Empty;
+
+        public ToastIcon Icon { get; set; } = ToastIcon.Default;
+
+        public ToastPosition Position { get; set; } = ToastPosition.TopRight;
+
+        public bool Persist { get; set; } = false;
+
+        public int Duration { get; set; } = 5000;
+
+        public CancellationTokenSource CancellationTokenSource { get; set; } = new CancellationTokenSource();
+    }
+}
