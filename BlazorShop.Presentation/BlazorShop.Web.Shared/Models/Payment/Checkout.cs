e042a10b9fdff16f99070fdef929f5a7635e9fa2
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-19 14:39:48 +0200

    Add payment processing models and related classes
    Introduces the `BlazorShop.Web.Shared.Models.Payment` namespace with several new classes:
    - `Checkout`: Manages payment method and cart items.
    - `CreateAchieve`: Inherits from `ProcessCart` and includes a `UserId`.
    - `GetPaymentMethod`: Represents a payment method with an ID and optional name.
    - `ProcessCart`: Defines properties for product ID and quantity for cart management.

---
+++ BlazorShop.Presentation/BlazorShop.Web.Shared/Models/Payment/Checkout.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorShop.Web.Shared.Models.Payment
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Checkout
+    {
+        [Required]
+        public Guid PaymentMethodId { get; set; }
+
+        [Required]
+        public IEnumerable<ProcessCart> Carts { get; set; } = [];
+    }
+}
