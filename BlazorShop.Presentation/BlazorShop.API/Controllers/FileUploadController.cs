1910e40c1dddcc4cd98d8862b51173af018c92c9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-21 17:16:36 +0200

    Add FileUploadController for handling file uploads
    Implemented a new `FileUploadController` in the `BlazorShop.API.Controllers` namespace. This controller provides an HTTP POST method `UploadFile` to manage file uploads, including validation, directory creation, file saving, and returning the file's URL upon successful upload.

---
+++ BlazorShop.Presentation/BlazorShop.API/Controllers/FileUploadController.cs
@@ -0,0 +1,49 @@
+﻿namespace BlazorShop.API.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/upload")]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        ///  Uploads a file to the server.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>A message and the URL of the uploaded file.</returns>
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\uploads");
+
+            if (!Directory.Exists(uploadsPath))
+            {
+                Directory.CreateDirectory(uploadsPath);
+            }
+
+            var filePath = Path.Combine(uploadsPath, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var fileUrl = $"{Request.Scheme}://{Request.Host}/uploads/{file.FileName}";
+
+            return Ok(new { Message = "File uploaded successfully.", FileUrl = fileUrl });
+        }
+    }
+}
