4482f63e032014f6eecbed5635511dc5b667d672
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-25 18:38:37 +0200

    Add About, Customer Service, FAQ, and Main Page features
    Created new pages for About Us, Customer Service, and FAQs, including their respective styles and logic. Implemented a promotional banner and updated the homepage layout. Added functionality to display products by category on the Main Page, with cart management features.

---
+++ BlazorShop.Presentation/BlazorShop.Web/Pages/Public/CustomerService.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorShop.Web.Pages.Public
+{
+    public partial class CustomerService
+    {
+        private string CustomerName { get; set; } = string.Empty;
+
+        private string CustomerEmail { get; set; } = string.Empty;
+
+        private string CustomerMessage { get; set; } = string.Empty;
+
+        private void SubmitTicket()
+        {
+            // Логика за обработка на заявката
+            Console.WriteLine($"Name: {this.CustomerName}, Email: {this.CustomerEmail}, Message: {this.CustomerMessage}");
+            this.CustomerName = string.Empty;
+            this.CustomerEmail = string.Empty;
+            this.CustomerMessage = string.Empty;
+        }
+    }
+}
