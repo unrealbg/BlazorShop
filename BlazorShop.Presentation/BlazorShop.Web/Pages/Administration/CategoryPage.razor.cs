b9304a8625bb4599e93b45cfd56060b3794db2a2
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-25 18:37:35 +0200

    Enhance navigation and layout across admin pages
    Updated AdminNavBar with a new header logo and mobile menu.
    Introduced AdminNavBar.razor.cs and .razor.css for menu management and styling.
    Refined layout in CategoryPage and ProductPage for better appearance.
    Implemented category and product management functionalities in their respective .razor.cs files.
    Streamlined SalesPage by removing unnecessary code and added order item retrieval logic.

---
+++ BlazorShop.Presentation/BlazorShop.Web/Pages/Administration/CategoryPage.razor.cs
@@ -0,0 +1,85 @@
+﻿namespace BlazorShop.Web.Pages.Administration
+{
+    using BlazorShop.Web.Shared.Models;
+    using BlazorShop.Web.Shared.Models.Category;
+    using BlazorShop.Web.Shared.Toast;
+
+    public partial class CategoryPage
+    {
+        private bool _showDialog = false;
+
+        private bool _showConfirmDeleteDialog = false;
+
+        private Guid _categoryToDelete;
+
+        private IEnumerable<GetCategory> _categories = [];
+
+        private CreateCategory _category = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await this.GetCategories();
+        }
+
+        private async Task GetCategories()
+        {
+            _categories = await this.CategoryService.GetAllAsync();
+        }
+
+        private void AddCategory()
+        {
+            _showDialog = true;
+        }
+
+        private void Cancel()
+        {
+            _showDialog = false;
+            _category = new CreateCategory();
+        }
+
+        private void ConfirmDelete(Guid id)
+        {
+            _categoryToDelete = id;
+            _showConfirmDeleteDialog = true;
+        }
+
+        private async Task DeleteCategoryConfirmed()
+        {
+            _showConfirmDeleteDialog = false;
+            var result = await this.CategoryService.DeleteAsync(_categoryToDelete);
+
+            if (result.Success)
+            {
+                await this.GetCategories();
+            }
+
+            this.ShowToast(result, "Delete-Category");
+        }
+
+        private void CancelDelete()
+        {
+            _showConfirmDeleteDialog = false;
+            _categoryToDelete = Guid.Empty;
+        }
+
+        private async Task SaveCategory()
+        {
+            var result = await this.CategoryService.AddAsync(_category);
+            if (result.Success)
+            {
+                _showDialog = false;
+                await this.GetCategories();
+            }
+
+            this.ShowToast(result, "Add-Category");
+        }
+
+        private void ShowToast(ServiceResponse result, string heading)
+        {
+            var level = result.Success ? ToastLevel.Success : ToastLevel.Error;
+            var icon = result.Success ? ToastIcon.Success : ToastIcon.Error;
+
+            this.ToastService.ShowToast(level: level, message: result.Message, heading: heading, iconClass: icon);
+        }
+    }
+}
