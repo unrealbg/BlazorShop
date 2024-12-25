686c8ac05c66351c5b3721e53326033e31f6cb5d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-12-25 18:36:50 +0200

    Add header, search, and side navigation components
    - Updated `HeaderBoxComponent` to include a cart link and user account dropdown with authentication-based options.
    - Enhanced `SearchBarComponent` with a typeahead search feature for products and navigation to search results.
    - Created a side navigation menu in `SideNavComponent` to display categories with a toggle functionality.
    - Managed user authentication state and category fetching in the respective component code-behind files.

---
+++ BlazorShop.Presentation/BlazorShop.Web/Components/Header/HeaderBoxComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorShop.Web.Components.Header
+{
+    using System.Security.Claims;
+
+    using BlazorShop.Web.Shared;
+
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Authorization;
+
+    public partial class HeaderBoxComponent
+    {
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthState { get; set; } = default!;
+
+        private bool IsAdmin;
+
+        private string UserEmail = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var user = (await this.AuthState).User;
+
+            this.IsAdmin = user.IsInRole(Constant.Administration.AdminRole);
+
+            var emailClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+            if (emailClaim != null)
+            {
+                this.UserEmail = emailClaim.Value;
+            }
+        }
+    }
+}
---
+++ BlazorShop.Presentation/BlazorShop.Web/Components/Header/SearchBarComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿namespace BlazorShop.Web.Components.Header
+{
+    using BlazorShop.Web.Shared.Models.Product;
+
+    using Microsoft.AspNetCore.Components;
+
+    public partial class SearchBarComponent
+    {
+        [Parameter]
+        public IEnumerable<GetProduct> Products { get; set; } = Enumerable.Empty<GetProduct>();
+
+        private GetProduct _selectedProduct = new();
+
+        public GetProduct SelectedProduct
+        {
+            get => this._selectedProduct;
+            set
+            {
+                if (this._selectedProduct != value)
+                {
+                    this._selectedProduct = value;
+                    this.OnSelectedProductChanged();
+                }
+            }
+        }
+
+        private void OnSelectedProductChanged()
+        {
+            if (this._selectedProduct == null)
+            {
+                return;
+            }
+
+            this.NavigationManager.NavigateTo($"search-result/{this._selectedProduct.Name}");
+        }
+
+        private Task<IEnumerable<GetProduct>> SearchProducts(string searchText)
+        {
+            var filtered = this.Products.Where(
+                x => x.Name!.Contains(searchText, StringComparison.CurrentCultureIgnoreCase) || x.Description!.Contains(
+                         searchText,
+                         StringComparison.CurrentCultureIgnoreCase));
+
+            return Task.FromResult(filtered);
+        }
+    }
+}
