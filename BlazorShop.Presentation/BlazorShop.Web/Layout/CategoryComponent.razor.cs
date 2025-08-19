e7d914d31a7f5ad767b72b53fce9aa2076d9413e
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-08-19 17:21:43 +0300

    Enhance category display and styling
    Updated `CategoryComponent.razor` to check for non-null and non-empty categories before rendering. Implemented horizontal scrolling with improved styling and added a message for empty categories. Introduced `GetColor(int index)` method in `CategoryComponent.razor.cs` for dynamic category styling. Added scrollbar-hiding styles in `CategoryComponent.razor.css` and made minor adjustments in `site.css` for consistent styling.

--- BlazorShop.Presentation/BlazorShop.Web/Layout/CategoryComponent.razor.cs
+++ BlazorShop.Presentation/BlazorShop.Web/Layout/CategoryComponent.razor.cs
@@ -17,5 +17,19 @@
                 this.ToastService.ShowErrorToast("An error occurred while loading categories.");
             }
         }
+
+        private (string bg, string ring) GetColor(int index)
+        {
+            var palette = new (string bg, string ring)[]
+            {
+                ("bg-rose-500","ring-rose-200"),
+                ("bg-amber-500","ring-amber-200"),
+                ("bg-emerald-500","ring-emerald-200"),
+                ("bg-sky-500","ring-sky-200"),
+                ("bg-violet-500","ring-violet-200"),
+                ("bg-fuchsia-500","ring-fuchsia-200"),
+            };
+            return palette[index % palette.Length];
+        }
     }
 }
