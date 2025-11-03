dc1dec69e7316e34796016a93ef5417e8509e763
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-11-03 18:47:55 +0200

    Add GetProductRecommendation DTO class
    Introduced a new namespace `BlazorShop.Application.DTOs.Product`
    and added the `GetProductRecommendation` class. This class
    encapsulates product recommendation data with properties for
    ID, name, image, price, and category name. Supports features
    for retrieving and displaying product recommendations.

---
+++ BlazorShop.Application/DTOs/Product/GetProductRecommendation.cs
@@ -0,0 +1,15 @@
+namespace BlazorShop.Application.DTOs.Product
+{
+    public class GetProductRecommendation
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Image { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? CategoryName { get; set; }
+    }
+}
